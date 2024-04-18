04b817b47a65e6749fce375aeee7215c70c226b3
Shoykot Amanat <shoykot.amanat@gmail.com>
2024-04-19 05:02:31 +0600

    automate currency and destination selection

--- booking/booking.py
+++ booking/booking.py
@@ -1,11 +1,15 @@
 import booking.constants as const
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
 class Booking(webdriver.Chrome):
     def __init__(self, teardown=False):
         super(Booking, self).__init__()
         self.teardown = teardown
+        self.implicitly_wait(15)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -13,4 +17,22 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
-        self.maximize_window()
+        dismis_signin_popup = self.find_element(By.CSS_SELECTOR, "button[aria-label='Dismiss sign-in info.']")
+        dismis_signin_popup.click()
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element(By.XPATH, "//span[contains(text(),'BDT')]")
+        currency_element.click()
+        selected_currency_element = self.find_element(
+            By.XPATH, f"//div[@data-testid='Suggested for you']//div[contains(@class,'ea1163d21f')][normalize-space()='{currency}']"
+        )
+        selected_currency_element.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element(By.ID, ":re:")
+        search_field.clear()
+        search_field.send_keys(place_to_go)
+        time.sleep(2)
+        first_result = self.find_element(By.ID, "autocomplete-result-0")
+        first_result.click()
+        
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from booking.booking import Booking
+import time
 
 with Booking() as bot:
     bot.land_first_page()
+    bot.change_currency(currency='EUR')
+    bot.select_place_to_go('New York')
+    time.sleep(5)
+    
