8cf140373f7c8105eab9991fefffe2fd9e9128e3
Shoykot Amanat <shoykot.amanat@gmail.com>
2024-04-20 04:23:18 +0600

    addded more functionality

--- booking/booking.py
+++ booking/booking.py
@@ -17,8 +17,8 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
-        dismis_signin_popup = self.find_element(By.CSS_SELECTOR, "button[aria-label='Dismiss sign-in info.']")
-        dismis_signin_popup.click()
+        # dismis_signin_popup = self.find_element(By.CSS_SELECTOR, "button[aria-label='Dismiss sign-in info.']")
+        # dismis_signin_popup.click()
 
     def change_currency(self, currency=None):
         currency_element = self.find_element(By.XPATH, "//span[contains(text(),'BDT')]")
@@ -35,4 +35,42 @@ class Booking(webdriver.Chrome):
         time.sleep(2)
         first_result = self.find_element(By.ID, "autocomplete-result-0")
         first_result.click()
+
+    def select_dates(self, check_in_date, check_out_date):
+        self.execute_script("window.scrollBy(0, 400);", "")
+        check_in_element = self.find_element(By.CSS_SELECTOR, f"span[data-date='{check_in_date}']")
+        # self.execute_script("arguments[0].scrollIntoView(true);", check_in_element)
+        check_in_element.click()
+        check_out_element = self.find_element(By.CSS_SELECTOR, f"span[data-date='{check_out_date}']")
+        # self.execute_script("arguments[0].scrollIntoView(true);", check_out_element)
+        check_out_element.click()
+
+    def select_adults(self, count=1):
+        selection_element = self.find_element(
+            By.CSS_SELECTOR, "span[data-testid='searchbox-form-button-icon']"
+        ) # find the fillup button for the adults
+        selection_element.click()
+        time.sleep(2)
+        default_adults = int(self.find_element(
+            By.XPATH, "//div[@id=':rf:']//div[@class='a7a72174b8'][1]//span[@class='d723d73d5f']"
+        ).text) # find the default number of adults  
+        print(default_adults)
+
+        # increase or decrease the number of adults
+        # based on the difference between the default number of adults and the desired number of adults
+
+        if default_adults < count:                 
+            for _ in range(count - default_adults):
+                self.find_element(
+                    By.XPATH, "//div[@id=':rf:']//div[@class='a7a72174b8'][1]//button[2]"
+                ).click()
+        elif default_adults > count:
+            for _ in range(default_adults - count):
+                self.find_element(
+                    By.XPATH, "//div[@id=':rf:']//div[@class='a7a72174b8'][1]//button[1]"
+                ).click()
+
+    def search(self):
+        search_button = self.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        search_button.click()
         
--- run.py
+++ run.py
@@ -3,7 +3,11 @@ import time
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.change_currency(currency='EUR')
+    bot.change_currency(currency='USD')
     bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date='2024-04-22', 
+                     check_out_date='2024-04-25')
+    bot.select_adults(count=10)
+    bot.search()
     time.sleep(5)
     
